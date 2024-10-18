aa595461f69b14c97dac10ef60da87893d1dc812
Morgan Jones <me@morganlabs.dev>
2024-10-18 21:20:29 +0100

    feat: Flake

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Flake to run NodeJS and NPM";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [ nodejs_20 ];
+      };
+    };
+}
